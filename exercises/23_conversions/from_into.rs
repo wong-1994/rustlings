51f0d9b64c2528ce5a14c2c2c8a9de606baa4bc6
1994 wong <seanwxj@tongji.edu.cn>
2023-12-29 09:28:34 +0000

    finish all exercise

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let personInfo:Vec<&str> = s.split(",").collect();
+        if personInfo.len() == 0 {
+            return Person::default();
+        } else if personInfo.len() == 1 {
+            match personInfo[0].parse::<usize>() {
+                Ok(age) => return Person {age, ..Person::default()},
+                _ => return Person::default(),
+            }
+        } else {
+            if personInfo[0].len() == 0 {
+                return Person::default();
+            }
+            let name = personInfo[0].to_string();
+            match personInfo[1].parse::<usize>() {
+                Ok(age) => return Person { name, age },
+                _ => return Person::default(),
+            }
+        }
     }
 }
 
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,7 +31,11 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+impl From<ParseIntError> for ParsePersonError {
+    fn from(err: ParseIntError) -> ParsePersonError {
+        ParsePersonError::ParseInt(err)
+    }
+}
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +56,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let personInfo:Vec<_> = s.split(',').collect();
+        if personInfo.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if personInfo[0].len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let name = personInfo[0].to_string();
+        let age = personInfo[1].parse::<usize>()?;
+        Ok(Person { name, age })
     }
 }
 
